bbdfff149633e6be3a99d5986d050d4a1e69292b
umebshii <ts24085@ts.hs.kr>
2025-06-20 03:00:11 +0000

    12

--- app_eda.py
+++ app_eda.py
@@ -210,7 +210,7 @@ class EDA:
             st.info("population_trends.csv 파일을 업로드 해주세요.")
             return
 
-        df = pd.read_csv(uploaded, parse_dates=['datetime'])
+        df = pd.read_csv(uploaded)
 
         tabs = st.tabs([
             "1. 기초 통계",
